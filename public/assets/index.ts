7b599ec3263927b5db4de0a50fac83135465473e
moses <timbwamoses83@gmail.com>
2023-05-21 00:34:34 +0300

    added icons and motion

---
+++ public/assets/index.ts
@@ -0,0 +1,7 @@
+import logo from "./images/logo.png";
+import mobile from "./images/mobile.png";
+import profile from "./images/profile.png";
+import softs from "./images/softs.png";
+import software from "./images/software.png";
+
+export {logo, mobile, profile, softs, software}
